229f3cd73ff1e74327810ea55f57b346c1135389
arissatya <arissatya.nugraha@gmail.com>
2020-08-06 18:28:43 +0700

    meals fetched

--- js/script.js
+++ js/script.js
@@ -7,6 +7,39 @@ function showRegister() {
 function showHome() {
 
 }
+function showAris() {
+  $('#aris').show()
+}
+function mealsRecomendation() {
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:3000/meals",
+    headers: {
+      access_token: localStorage.getItem('access_token')
+    }
+  })
+    .done(response => {
+      // console.log(response)
+      $('#appendMeals').append(`
+      <h2>${response.name}</h2><br>
+      <h4>${response.type}</h4><br>
+      <table>
+        <tr>
+        <td><img src="${response.thumb}" width="500" height="300"></td>
+        </tr>
+        <tr>
+        <td>${response.instruction}</td>
+        </tr>
+      </table>
+      `)
+    })
+    .fail(xhr => {
+      console.log(xhr);
+    })
+    .always(_ => {
+      console.log('MASUK');
+    })
+}
 
 $(document).ready(function () {
   if (!localStorage.getItem('access_token')) {
@@ -14,6 +47,8 @@ $(document).ready(function () {
   } else {
     showHome()
   }
+
+  $('#fecthMeals').on('click', mealsRecomendation)
 
   $('#btnGoToRegisterPage').click(function (event) {
     event.preventDefault()
